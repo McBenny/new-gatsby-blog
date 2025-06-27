2e28c7f728d8ed523189872998e38a0cf3eecb0d
Ben Adam <benoit.adam@yacare.fr>
2025-06-27 12:00:51 +1000

    First changes: markdown support, list of exceprts.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
@@ -69,9 +69,24 @@ const moreLinks = [
 
 const utmParameters = `?utm_source=starter&utm_medium=start-page&utm_campaign=default-starter`
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => {
+  console.log(data);
+  
+  return (
   <Layout>
-    <div className={styles.textCenter}>
+    <div>
+      <h1>Ben's thoughts...</h1>
+      <h4>{data.allMarkdownRemark.totalCount}</h4>
+      {
+        data.allMarkdownRemark.edges.map(({node}) => (
+          <div key={node.id}>
+            <span>{node.frontmatter.title} - { node.frontmatter.date }</span>
+            <p>{ node.excerpt }</p>
+          </div>
+        ))
+      }
+    </div>
+    {/* <div className={styles.textCenter}>
       <StaticImage
         src="../images/example.png"
         loading="eager"
@@ -95,8 +110,8 @@ const IndexPage = () => (
         <br />
         Edit <code>src/pages/index.js</code> to update this page.
       </p>
-    </div>
-    <ul className={styles.list}>
+    </div> */}
+    {/* <ul className={styles.list}>
       {links.map(link => (
         <li key={link.url} className={styles.listItem}>
           <a
@@ -108,15 +123,16 @@ const IndexPage = () => (
           <p className={styles.listItemDescription}>{link.description}</p>
         </li>
       ))}
-    </ul>
-    {moreLinks.map((link, i) => (
+    </ul> */}
+    {/* {moreLinks.map((link, i) => (
       <React.Fragment key={link.url}>
         <a href={`${link.url}${utmParameters}`}>{link.text}</a>
         {i !== moreLinks.length - 1 && <> · </>}
       </React.Fragment>
-    ))}
+    ))} */}
   </Layout>
 )
+}
 
 /**
  * Head export to define metadata for the page
@@ -126,3 +142,22 @@ const IndexPage = () => (
 export const Head = () => <Seo title="Home" />
 
 export default IndexPage
+
+export const query = graphql`
+  query {
+    allMarkdownRemark {
+      edges {
+        node {
+          id
+          excerpt
+          frontmatter {
+            description
+            date(locale: "")
+            title
+          }
+        }
+      }
+      totalCount
+    }
+  }
+`
